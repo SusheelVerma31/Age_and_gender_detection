f64e039d0e7a908ea1bcf9345248895857303aa9
SusheelVerma31 <150130897+SusheelVerma31@users.noreply.github.com>
2024-04-22 09:43:27 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,90 @@
+# Flask App
+
+import cv2
+from flask import Flask, render_template, Response
+
+app=Flask(__name__, template_folder= 'template')
+video = cv2.VideoCapture(0)
+
+def faceBox(faceNet, frame):
+    frameWidth = frame.shape[1]
+    frameHeight = frame.shape[0]
+    blob = cv2.dnn.blobFromImage(frame, 1.0, (277, 277), [104, 117, 123], swapRB=False)
+    faceNet.setInput(blob)
+    detection = faceNet.forward()
+    bboxs = []
+    for i in range(detection.shape[2]):
+        confidence = detection[0, 0, i, 2]
+        if confidence>0.7:
+            x1 = int(detection[0, 0, i, 3] * frameWidth)
+            y1 = int(detection[0, 0, i, 4] * frameWidth)
+            x2 = int(detection[0, 0, i, 5] * frameWidth)
+            y2 = int(detection[0, 0, i, 6] * frameHeight)
+            bboxs.append([x1, y1, x2, y2])
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 1)
+    return frame, bboxs
+
+faceProto = "opencv_face_detector.pbtxt"
+faceModel = "opencv_face_detector_uint8.pb"
+
+ageProto = "age_deploy.prototxt"
+ageModel = "age_net.caffemodel"
+
+genderProto = "gender_deploy.prototxt"
+genderModel = "gender_net.caffemodel"
+
+faceNet = cv2.dnn.readNet(faceModel, faceProto)
+ageNet = cv2.dnn.readNet(ageModel, ageProto)
+genderNet = cv2.dnn.readNet(genderModel, genderProto)
+
+MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
+ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
+genderList = ['Male', 'Female']
+
+# video = cv2.VideoCapture(0)
+padding = 20
+def gen_frames():
+    while True:
+        ret, frame = video.read()
+        frame, bboxs = faceBox(faceNet, frame)
+        for bbox in bboxs:
+            # face=frame[bbox[1]:bbox[3], bbox[0]:bbox[2]]
+            face = frame[max(0, bbox[1] - padding):min(bbox[3] + padding, frame.shape[0] - 1),
+                   max(0, bbox[0] - padding):min(bbox[2] + padding, frame.shape[1] - 1)]
+            blob = cv2.dnn.blobFromImage(face, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
+            genderNet.setInput(blob)
+            genderPred = genderNet.forward()
+            gender = genderList[genderPred[0].argmax()]
+
+            ageNet.setInput(blob)
+            agePred = ageNet.forward()
+            age = ageList[agePred[0].argmax()]
+
+            label = "{},{}".format(gender, age)
+            cv2.rectangle(frame, (bbox[0], bbox[1] - 30), (bbox[2], bbox[1]), (0, 255, 0), -1)
+            cv2.putText(frame, label, (bbox[0], bbox[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2,
+                        cv2.LINE_AA)
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/')
+def index():
+     return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+    #    cv2.imshow("Age-Gender",frame)
+#
+#     k=cv2.waitKey(1)
+#     if k==ord('q'):
+#         break
+# video.release()
+# cv2.destroyAllWindows()
